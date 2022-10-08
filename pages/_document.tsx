96bf7452367c3e2a8b4945914362bb88b114daf5
shaoxuan0916 <shaoxuanhinhua@gmail.com>
2022-10-08 23:49:21 +0800

    add google ads test

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5HYGEXR6WC"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+          }}
+        />
+      </body>
+    </Html>
+  )
+}
